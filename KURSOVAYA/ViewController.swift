2591833954cfebc0341c634073f3fed9fa42dc10
vvatervvater <professionalhunter@ukr.net>
2021-01-03 19:28:20 +0200

    Initial Commit

---
+++ KURSOVAYA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KURSOVAYA
+//
+//  Created by Denis Ravkin on 03.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
